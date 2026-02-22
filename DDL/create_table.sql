0b24ff05005576d17bc9c844e8e53f11cb1caf4d
Amol Patil <amolp6143@gmail.com>
2026-02-22 10:48:42 +0530

    deartment table added to the ddl

--- DDL/create_table.sql
+++ DDL/create_table.sql
@@ -5,5 +5,13 @@ create table employee(
     age int not null,
     department_id int not null,
     location varchar(255) default('pune'),
-    primary key(id)
+    primary key(id),
+    foreign key(department_id) references department(dept_id)
+);
+
+-- create table named department
+create table department(
+    dept_id int unique,
+    dept_name varchar(255) not null,
+    primary key(dept_id)
 );
